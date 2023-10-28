c4209c344dadee7f38023bad22da1410849c03b5
yuckylot <luckyfaceit007@gmail.com>
2023-10-29 00:28:55 +0600

    add lab1

---
+++ Algorithmization/Lab1 C#/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Lab1
+{
+    class Programm
+    {
+        public static void Main()
+        {
+            double a = 0.5;
+            double b = 2;
+
+
+            for (int i = 0; i < 2; i++)
+            {
+                Console.WriteLine("Введите х{0}:",i+1);
+                double x = Convert.ToDouble(Console.ReadLine());
+
+                double y;
+
+                y = (b * Math.Pow(x, 2) * Math.Pow(Math.Exp(1), a * Math.Pow(x, 2))) + (a * Math.Pow(x + 1.5, 0.5));
+
+                Console.WriteLine(y);
+
+            }
+        }
+    }
+}
