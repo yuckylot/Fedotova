34fc0a1d3b57bae90fa708aa52ac34b0e0962b94
yuckylot <luckyfaceit007@gmail.com>
2023-12-11 12:19:07 +0600

    add solution for string

---
+++ Algorithmization/11.12.23/Program.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+using System.Security.AccessControl;
+using System.Linq;
+
+namespace gg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Obraz");
+            string obr = Console.ReadLine();
+            Console.WriteLine("Enter count of strings");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter string");
+            int sums = 0;
+            for (int i = 0; i < n; i++)
+            {
+                string srr = Console.ReadLine();
+                string sel = srr.Replace(obr, "&");
+                int dep = sel.Count(p => p == '&');
+                sums += dep;
+            }
+            Console.WriteLine(sums);
+        }
+    }
+}
