de9362502257458cf89c3f7945da29f99d85e81c
yuckylot <luckyfaceit007@gmail.com>
2024-01-12 21:02:52 +0600

    dolgs

---
+++ Algorithmization/26.10.23/Program.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Linq;
+
+// Задано множество из N элементов
+// Необходимо сформировать одномерные массивы, которые хранят:
+// 1. Пересечение всех множеств
+// 2. Объединение всех множеств
+// 3. Дополнение для каждого множества относительно объединения
+class Sets
+{
+    public static int[] SelectionSort(int[] array)
+    {
+        int[] sorted = new int[array.Length];
+        for (int i = 0; i < array.Length; i++)
+        {
+            int min = Int32.MaxValue;
+            for (int j = i; j < array.Length; j++)
+            {
+                if (array[j] < min)
+                {
+                    min = array[j];
+                    int temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
+            }
+            sorted[i] = min;
+        }
+
+        return sorted;
+    }
+
+    public static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+        int[][] arr = new int[n][];
+        int length = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            int m = Convert.ToInt32(Console.ReadLine());
+            length += m;
+            arr[i] = new int[m];
+            for (int j = 0; j < m; j++)
+            {
+                arr[i][j] = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+
+        int[] union = new int[length];
+        int countUnion = 0;
+
+        Console.WriteLine("Массивы:");
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < arr[i].Length; j++)
+            {
+                Console.Write($"{arr[i][j].ToString().PadLeft(5, ' ')} ");
+                if (!union.Contains(arr[i][j]))
+                {
+                    union[countUnion] = arr[i][j];
+                    countUnion++;
+                }
+            }
+            Console.WriteLine();
+        }
+
+        union = new int[countUnion];
+        int pointer = 0;
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < arr[i].Length; j++)
+            {
+                if (!union.Contains(arr[i][j]))
+                {
+                    union[pointer] = arr[i][j];
+                    pointer++;
+                }
+            }
+        }
+
+        union = SelectionSort(union);
+
+        int[] intersections = new int[union.Length];
+        int countIntersection = 0;
+        for (int i = 0; i < intersections.Length; i++)
+        {
+            intersections[i] = 0;
+        }
+
+        for (int k = 0; k < union.Length; k++)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                if (arr[i].Contains(union[k]))
+                {
+                    intersections[k]++;
+                }
+            }
+        }
+
+        for (int i = 0; i < intersections.Length; i++)
+        {
+            if (intersections[i] == n)
+            {
+                countIntersection++;
+            }
+        }
+
+        int[] intersection = new int[countIntersection];
+        pointer = 0;
+        for (int i = 0; i < intersections.Length; i++)
+        {
+            if (intersections[i] == n)
+            {
+                intersection[pointer] = union[i];
+                pointer++;
+            }
+        }
+
+        Console.WriteLine("Пересечение всех множеств:");
+        if (countIntersection > 0)
+        {
+            foreach (int item in intersection)
+            {
+                Console.Write($"{item} ");
+            }
+        }
+        else
+        {
+            Console.Write("Пусто!");
+        }
+        Console.WriteLine();
+
+        Console.WriteLine("Объединение всех множеств:");
+        if (countUnion > 0)
+        {
+            foreach (int item in union)
+            {
+                Console.Write($"{item} ");
+            }
+        }
+        else
+        {
+            Console.Write("Пусто!");
+        }
+        Console.WriteLine();
+
+        Console.WriteLine("Дополнение для каждого множества относительно объединения:");
+        for (int i = 0; i < n; i++)
+        {
+            int flag = 0;
+            Console.Write($"{i}:\t");
+            for (int k = 0; k < union.Length; k++)
+            {
+                if (!arr[i].Contains(union[k]))
+                {
+                    Console.Write("{0} ", union[k]);
+                    flag = 1;
+                }
+            }
+            if (flag == 0)
+            {
+                Console.Write("Пусто!");
+            }
+            Console.WriteLine();
+        }
+    }
+}
--- Algorithmization/30.10.23 first/Program.cs
+++ Algorithmization/30.10.23 first/Program.cs
@@ -1,51 +1,92 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Linq;
 
-//
-//
-//
-
-namespace cat_n_mice 
-{ 
-    class Programm
+// Дается n серых мышек и m белых,
+// k - мышка, которую съедает кот,
+// n1 - остаток серых,
+// m1 - остаток белых.
+// Вывод: выход с серой мышки.
+class MiceAndCat
+{
+    public static void Main(string[] args)
     {
-        public static void Main()
+        int n = Convert.ToInt32(Console.ReadLine());
+        int m = Convert.ToInt32(Console.ReadLine());
+        int k = Convert.ToInt32(Console.ReadLine());
+        int n1 = Convert.ToInt32(Console.ReadLine());
+        int m1 = Convert.ToInt32(Console.ReadLine());
+
+        int remainder = n1 + m1;
+
+        int[] mice = new int[n + m];
+        for (int i = 0; i < mice.Length; i++)
         {
-            Console.WriteLine("Введите кол-во серых:");
-            int n = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Введите кол-во белых:");
-            int m = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Введите Укус:");
-            int k = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("n1:");
-            int n1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("m1:");
-            int m1 = Convert.ToInt32(Console.ReadLine());
-            
-            if (n > 0 & n1 < n & m1 < m)
+            mice[i] = 1;
+        }
+
+        int pointer = 0;
+
+        while (mice.Count(c => c == 1) > remainder)
+        {
+            int moved = 0;
+
+            while (moved != k)
             {
-                List<int> mice = new List<int>();
-                for (int i = 0; i <n; i++)
+                ++pointer;
+
+                if (pointer > mice.Length - 1)
                 {
-                    mice.Add(1);
-                }
-                for (int i = 0; i < m; i++)
-                {
-                    mice.Add(2);
+                    pointer = 0;
                 }
 
-                Console.Write("[ ");
-                foreach (int i in mice)
+                if (mice[pointer] == 1)
                 {
-                    Console.Write(i + " ");
+                    ++moved;
                 }
-                Console.Write("]");
-
-
             }
-            else
+
+            mice[pointer] = 0;
+        }
+
+        int placed_n = 0;
+        int placed_m = 0;
+        int placed_alive_n = 0;
+        int placed_alive_m = 0;
+
+        if (n - n1 == 0 || m - m1 == 0)
+        {
+            Console.WriteLine("Мышек так расположить не получится");
+        }
+        else
+        {
+            for (int i = 0; i < mice.Length; i++)
             {
-                Console.WriteLine("syntax error");
+                if (mice[i] == 0)
+                {
+                    if (placed_n < n - n1)
+                    {
+                        Console.WriteLine("Серая");
+                        placed_n++;
+                    }
+                    else if (placed_m < m - m1)
+                    {
+                        Console.WriteLine("Белая");
+                        placed_m++;
+                    }
+                }
+                else if (mice[i] == 1)
+                {
+                    if (placed_alive_n < n1)
+                    {
+                        Console.WriteLine("Серая");
+                        placed_alive_n++;
+                    }
+                    else if (placed_alive_m < m1)
+                    {
+                        Console.WriteLine("Белая");
+                        placed_alive_m++;
+                    }
+                }
             }
         }
     }
