62494c892ff69690dae442f013fd81d486d01cbe
yuckylot <luckyfaceit007@gmail.com>
2024-05-07 11:54:38 +0600

    ggd

---
+++ Algorithmization/Semester_2/SubString/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApplication23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader(@"./../../../subString.txt");
+            int min_count = int.MaxValue;
+            string min_line = "";
+            bool check = false;
+            while (true)
+            {
+                string line = sr.ReadLine();
+                if (line != null)
+                {
+                    int count = 0;
+                    foreach (char symb in line)
+                    {
+                        if (symb == 'a')
+                        {
+                            count += 1;
+                        }
+                        else if (count != 0 && count < min_count)
+                        {
+                            min_count = Math.Min(min_count, count);
+                            min_line = line;
+                            count = 0;
+                            check = true;
+                        }
+                    }
+                }
+                else
+                {
+                    break;
+                }
+            }
+            if (check)
+            {
+                Console.WriteLine(min_line);
+            }
+            else
+            {
+                Console.WriteLine("no a");
+            }
+            Console.ReadKey();
+        }
+    }
+}
