1588ab52a14ab592b7c84498b66787e423012614
yuckylot <luckyfaceit007@gmail.com>
2024-05-27 10:01:31 +0600

    All task complite

---
+++ Algorithmization/Semester_2/BancAccount/Program.cs
@@ -0,0 +1,136 @@
+﻿//Даны данные о банковских счетах клиентов
+//Структура данных: номер счёта, ФИО, доход, расход,
+//налог(5 % с дохода, автоматически, исходя из дохода).
+//  Необходимо с использованием запросов отобрать клиентов с отрицательным балансом,
+//  Посчитать количество клиентов с положительным балансом,
+//  Выдать клиентов с максимальным доходом,
+//  Посчитать общую сумму налогов
+
+using System;
+using System.CodeDom.Compiler;
+using System.Linq;
+
+namespace Yuckylot
+{
+    class Bank
+    {
+        string Name;
+        List<Account> Accounts;
+
+        public Bank()
+        {
+            Name = "Yuckylot Bank";
+            Accounts = new List<Account>();
+        }
+        public void FillBase()
+        {
+            for (int i = 1; i < 50; i ++)
+            {
+                Accounts.Add(GenerateAccount(i));
+            }
+        }
+        public Account GenerateAccount(int id)
+        {
+            string[] names1 = { "Серхио", "Даниэль", "Каролина", "Дэвид", "Рейна", "Сол", "Бернард", "Дэнни", "Димас", "Юрий", "Иван", "Лаура" };
+            string[] names2 = { "Тапиа", "Гутьеррес", "Руэда", "Гальвиз", "Юли", "Ривера", "Мамами", "Сауседо", "Домингес", "Эскобар", "Мартин", "Креспо" };
+            string[] names3 = { "Джонсон", "Уильямс", "Джонс", "Браун", "Дэвид", "Миллер", "Уилсон", "Андерсон", "Томас", "Джексон", "Уайт", "Дэвид", "Миллер", "Уилсон", "Андерсон", "Томас", "Джексон", "Уайт", "Робинсон" };
+            Random rnd = new Random();
+            return new Account(id, $"{names1[rnd.Next(0, names1.Length)]} {names2[rnd.Next(0, names2.Length)]} {names3[rnd.Next(0, names3.Length)]}", Math.Round(rnd.NextDouble()*10000, 2), Math.Round(rnd.NextDouble() * 10000, 2)); 
+        }
+        public void Show()
+        {
+            foreach (Account account in Accounts)
+            {
+                account.ShowAcc();  
+            }
+        }
+        public void NegativeBalanceAccounts()
+        {
+            Console.WriteLine("Чтобы увидеть аккаунты с Отрицательным балансом, нажмите любую калвишу");
+            Console.ReadKey();
+            Console.Clear();
+            Console.WriteLine("Счета с отрицательным Балансом");
+            Accounts
+                .Where(acc => (acc.Income - acc.Outcome - acc.Tax) < 0)
+                .ToList()
+                .ForEach(acc => acc.ShowAcc());
+
+        }
+        public void CountPositivBalanceAccounts()
+        {
+            Console.WriteLine("Чтобы увидеть количество счетов с положительным балансом, нажмите любую калвишу");
+            Console.ReadKey();
+            Console.Clear();
+            Console.WriteLine("Колличество счетов Балансом");
+            Console.WriteLine(
+                Accounts
+                .Where(acc => (acc.Income - acc.Outcome - acc.Tax) > 0)
+                .ToList()
+                .Count()
+                );
+                
+        }
+        public void ClientsWithMaxIncome()
+        {
+            Console.WriteLine("Чтобы увидеть клиентов с максимальным доходом, нажмите любую калвишу");
+            Console.ReadKey();
+            Console.Clear();
+            Console.WriteLine("Колличество счетов Балансом");
+            double maxIncome =
+                Accounts
+                .OrderByDescending(acc => acc.Income)
+                .First().Income;
+            Accounts
+                .Where(acc => acc.Income == maxIncome)
+                .ToList()
+                .ForEach(acc => Console.WriteLine($"На счеиту:{acc.Id}; Хозяин счета:{acc.Name}; Доходы: {acc.Income}"));
+        }
+        public void AllTaxSum()
+        {
+            Console.WriteLine("Чтобы увидеть сумму всех налогов, нажмите любую калвишу");
+            Console.ReadKey();
+            Console.Clear();
+            Console.Write("Сумма налогов: ");
+            double sum =
+                Accounts
+                .Select(acc => acc.Tax)
+                .ToList()
+                .Sum();
+            Console.Write(sum);
+        }
+    }
+    class Account
+    {
+        public int Id;
+        public string Name;
+        public double Income;
+        public double Outcome;
+        public double Tax;
+
+        public Account(int id, string name, double income, double outcome)
+        {
+            Id = id;
+            Name = name;
+            Income = income;
+            Outcome = outcome;
+            Tax = Math.Round(Income * 0.05, 2);
+        }
+        public void ShowAcc()
+        {
+            Console.WriteLine($"На счеиту:{Id}; Баланс: {Income - Outcome - Tax}; Хозяин счета:{Name}; Доходы: {Income}; Расходы: {Outcome}; Налог: {Tax}");
+        }
+    }
+
+    class Program
+    {
+        public static void Main() 
+        {
+            Bank bank = new Bank();
+            bank.FillBase();
+            bank.NegativeBalanceAccounts();
+            bank.CountPositivBalanceAccounts();
+            bank.ClientsWithMaxIncome();
+            bank.AllTaxSum();
+        }
+    }
+}
---
+++ Algorithmization/Semester_2/Workers/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+class Worker
+{
+    public int id;
+    public string name;
+    public string position;
+    public int salary;
+    public string category;
+    public int quantity;
+    public int pricePerPiece;
+
+    public Worker(int id, string name, string position, int salary, string category, int quantity, int pricePerPiece)
+    {
+        this.id = id;
+        this.name = name;
+        this.position = position;
+        this.salary = salary;
+        this.category = category;
+        this.quantity = quantity;
+        this.pricePerPiece = pricePerPiece;
+    }
+    public void Print()
+    {
+        Console.WriteLine($"id {id}");
+        Console.WriteLine($"name {name}");
+        Console.WriteLine($"position {position}");
+        Console.WriteLine($"salary {salary}");
+        Console.WriteLine($"category {category}");
+        Console.WriteLine($"quatity {quantity}");
+        Console.WriteLine($"ppp {pricePerPiece}");
+        Console.WriteLine($"q*p = {quantity * pricePerPiece}");
+        Console.WriteLine("");
+    }
+}
+
+public static class Program
+{
+    public static void Main()
+    {
+        List<Worker> workers = new List<Worker>(){
+            new Worker(1, "1", "1", 10000, "food", 23, 1000),
+            new Worker(2, "2", "2", 12000, "tech", 13, 1500),
+            new Worker(3, "3", "3", 15000, "food", 33, 200),
+        };
+
+        var q1 = from w in workers
+                 where w.salary < w.quantity * w.pricePerPiece
+                 select w;
+
+        Console.WriteLine($"1) {q1.Count()}");
+
+        Console.WriteLine($"2) ");
+        var q2 = from w in workers
+                 group w by w.category;
+
+        foreach (var category in q2)
+        {
+            int totalUnits = 0;
+            int totalMoney = 0;
+            foreach (var w in category) { totalUnits += w.quantity; totalMoney += w.quantity * w.pricePerPiece; }
+            Console.WriteLine($"{category.Key} - {totalUnits} un. / {totalMoney}$");
+        }
+
+        int q3 = workers.Select(x => x.quantity).Sum();
+        Console.WriteLine($"3) {q3}");
+
+        var q4 = workers.Where(w => w.salary > 0.5 * w.quantity * w.pricePerPiece).ToList().Count();
+        Console.WriteLine($"4) {q4}");
+    }
+}
